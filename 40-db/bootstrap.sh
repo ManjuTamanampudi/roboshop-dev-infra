1cedc056b28ad2034abc2a9f016210bfd93df96e
Manju Tamanampudi <manjukarri3@gmail.com>
2026-04-13 12:22:46 +0530

    one more commit

--- 40-db/bootstrap.sh
+++ 40-db/bootstrap.sh
@@ -6,10 +6,10 @@ environment =$2
 
 dnf install ansible -y
 
-cd home/ec2-user
+cd /home/ec2-user
 git clone https://github.com/ManjuTamanampudi/ansible-roboshop-roles-tf.git
 cd ansible-roboshop-roles-tf
 git pull
 
 
-ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+ansible-playbook -e component=mongodb -e env=dev roboshop.yaml
