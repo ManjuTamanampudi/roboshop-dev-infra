b71154097c879f4d521004960a0ef454320886c7
Manju Tamanampudi <manjukarri3@gmail.com>
2026-05-03 19:09:35 +0530

    one more commit

---
+++ 60-catalogue/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+component=$1
+environment=$2
+app_version=$3
+dnf install ansible -y
+
+cd /home/ec2-user
+git clone https://github.com/ManjuTamanampudi/ansible-roboshop-roles-tf.git
+cd ansible-roboshop-roles-tf
+git pull
+# if [ ! -d "ansible-roboshop-roles-tf" ]; then
+#   git clone https://github.com/ManjuTamanampudi/ansible-roboshop-roles-tf.git
+# else
+#   cd ansible-roboshop-roles-tf && git pull
+# fi
+
+
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
