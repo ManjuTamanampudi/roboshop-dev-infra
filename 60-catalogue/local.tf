e8710c5ad14f4abdb8d59fa54ceda130af5a5b30
Manju Tamanampudi <manjukarri3@gmail.com>
2026-05-03 18:24:38 +0530

    one more commit

---
+++ 60-catalogue/local.tf
@@ -0,0 +1,12 @@
+locals {
+  ami_id= data.aws_ami.joindevops.id
+   private_subnet_id= split(",",data.aws_ssm_parameter.db_subnet_id.value)[0]
+   catalogue_sg_id =data.aws_ssm_parameter.catalogue_sg_id.value
+   vpc_id = data.aws_ssm_parameter.vpc_id.value
+   common_tags={
+    Project= var.Project
+    Environment =var.Environment
+    Terraform = true
+  }
+  backend_alb_listener_arn =data.aws_ssm_parameter.backend_alb_listener_arn.value
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,194 @@
+resource "aws_instance" "catalogue" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = local.private_subnet_id
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.Project}-${var.Environment}-catalogue"
+  })
+}
+
+resource "terraform_data" "bootstrap" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+  }
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh catalogue ${var.Environment}",
+    ]
+  }
+
+}
+
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on  = [aws_instance.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.Project}-${var.Environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on         = [aws_ec2_instance_state.catalogue]
+}
+
+
+resource "aws_lb_target_group" "catalogue" {
+
+  name                 = "${var.Project}-${var.Environment}-catalogue"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 60
+  health_check {
+    enabled             = true
+    path                = "/health"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 2
+    interval            = 10
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    matcher             = "200-299"
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.Project}-${var.Environment}-catalogue"
+  })
+}
+
+resource "aws_launch_template" "catalogue" {
+  name                                 = "${var.Project}-${var.Environment}-catalogue"
+  image_id                             = aws_ami_from_instance.catalogue.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = "t3.micro"
+  vpc_security_group_ids               = [local.catalogue_sg_id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.Project}-${var.Environment}-catalogue"
+    })
+  }
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name = "${var.Project}-${var.Environment}-catalogue"
+    })
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.Project}-${var.Environment}-catalogue"
+  })
+
+}
+
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.Project}-${var.Environment}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = false
+
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = [local.private_subnet_id]
+  target_group_arns   = aws_lb_target_group.catalogue.arn
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+
+  dynamic "tag" {
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${var.Project}-${var.Environment}-catalogue"
+    })
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${var.Project}-${var.Environment}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 70.0
+  }
+
+}
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = local.backend_alb_listener_arn
+  priority     = 10
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+  condition {
+    host_header {
+      values = ["catalogue.backend-alb.${var.Environment}.${var.domain_name}"]
+
+    }
+  }
+}
+
+resource "terraform_data" "catalogue_delete" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  
+  depends_on = [ aws_autoscaling_policy.catalogue ]
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+  }
+}
